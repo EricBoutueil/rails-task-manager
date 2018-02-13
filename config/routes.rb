db4b2676225e3a7b8b74714fa6a5b25a3bcb6b5f
Eric Boutueil <e.boutueil@gmail.com>
2018-02-13 16:15:43 +0100

    methods edit and update done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get "tasks/:id", to: "tasks#show", as: :task
 
+  get    "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch  "tasks/:id",      to: "tasks#update"
 
   # get    "restaurants",          to: "restaurants#index"
 
